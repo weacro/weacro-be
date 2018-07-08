f86f579cdbeac092e429e042112791cc075a8754
Anatoliy Zaslavskiy <adz@nycitt.com>
2018-07-08 09:42:44 -0700

    wip

---
+++ src/PoseLoader.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { store_user, store_poses } from './store/actions/actions';
+import api from './API';
+import LoadDisplay from './Components/UI/Loader';
+
+class PoseLoader extends Component {
+	componentDidMount = async () => {
+	  const { UserLogin, storePoses } = this.props;
+	  UserLogin(api.user.get(true));
+	  storePoses(api.poses.get(true));
+	  UserLogin(await api.user.get());
+	  storePoses(await api.poses.get());
+	};
+
+	render = () => {
+	  const { poses, children } = this.props;
+	  if (!poses || !poses.length) {
+	    return <LoadDisplay />;
+	  }
+	  return (
+			<div>
+        {children}
+      </div>
+	  );
+	};
+}
+
+const mapStateToProps = ({ poose: { poses } }) => ({ poses });
+const mapDispatchToProps = dispatch => ({
+  UserLogin: user => dispatch(store_user(user)),
+  storePoses: pose => dispatch(store_poses(pose)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PoseLoader);
